c3e4f1b5ba9073babc3a275db1def821299e390a
JuanCruz771 <gabrielcruz.juan.771@gmail.com>
2025-01-26 23:36:12 -0300

    finalizado

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,27 +1,47 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = [];
 let nomeAmigo = document.getElementById('amigo');
 let resultado = document.getElementById('resultado');
+let lista = document.getElementById('listaAmigos');
+let validacao = false;
+
 function adicionarAmigo(){
-    
-    
-        
+
+    resultado.innerHTML = "";
     if(nomeAmigo.value == ''){
 
-        alert("Por favor Digite um nome");      
-    }
-    amigos.push(nomeAmigo.value);
-    document.getElementById('listaAmigos').innerHTML = amigos;
-    nomeAmigo.value = '';    
-}
+        alert("Por favor Digite um nome"); 
 
+    }else if(amigos.includes(nomeAmigo.value)){
+
+        alert("esse nome ja esta adicionado");
+        nomeAmigo.value = ''; 
+
+    } else {
+            amigos.push(nomeAmigo.value);
+            exibirNome();   
+            nomeAmigo.value = '';  
+        }      
+    }      
+
+function exibirNome(){
+    lista.innerHTML = '';
+    for(let i = 0; i < amigos.length; i++) {
+        lista.innerHTML += `<li>${amigos[i]}</li>`;
+    }
+
+}
 function sortearAmigo() {
 
-        let numeroAleatorio = Math.floor(Math.random() * amigos.length);    
+    if(amigos == ""){
+        alert("Por favor Digite um nome");
+    }
+    else{
+        let numeroAleatorio = Math.floor(Math.random() * amigos.length); 
         resultado.innerHTML = "seu amigo secreto é: " + amigos[numeroAleatorio]; 
-        document.getElementById('listaAmigos').innerHTML = '';
+        lista.innerHTML = '';
         amigos = [];
-    
+        
+    }
 }
 
     
